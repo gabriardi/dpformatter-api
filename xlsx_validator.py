b9073c74d5d954bc8c6a52855d4f9aefd42ffb90
Gabriele Ardizzone <gabri.ardi@gmail.com>
2021-10-06 16:15:18 +0200

    add worksheet validation

--- xlsx_validator.py
+++ xlsx_validator.py
@@ -12,4 +12,21 @@ def is_xlsx_base64(xlsx_base64):
 
 
 def is_worksheet_valid(ws):
-    return True
+    is_valid = True
+
+    identifiers = [
+        {"name": "Diensteinteilervorschlag", "number": 0},
+        {"name": "TOZ", "number": 0},
+        {"name": "Total bis Saisonende", "number": 0},
+        {"name": "Kommentar", "number": 0},
+    ]
+    for row in ws.iter_rows():
+        for cell in row:
+            for identifier in identifiers:
+                if identifier["name"] in str(cell.value):
+                    identifier["number"] += 1
+    for identifier in identifiers:
+        if identifier["number"] == 0:
+            is_valid = False
+
+    return is_valid
